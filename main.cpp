f2e2e5abfd04ee827b3bd90281de5625b6d9ba04
Timothy Utsab Bandyopadhyay <timothy.utsab@gmail.com>
2026-03-28 16:48:11 +0530

    multiple images

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include <QPixmap>
 #include <QResizeEvent>
 #include <QWheelEvent>
+#include <QKeyEvent>
 #include <QFileInfo>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <QScrollArea>
 #include <QTimer>
+#include <QPainter>
+#include <QStringList>
+#include <QDir>
 
 class ImageViewer : public QWidget {
 public:
@@ -41,18 +45,34 @@ public:
         bottomBar->setStyleSheet("background-color: #2b2b2b; color: white;");
         auto *bottomLayout = new QHBoxLayout(bottomBar);
         
+        auto *btnPrev = new QPushButton("Previous");
+        auto *btnNext = new QPushButton("Next");
         auto *btnFit = new QPushButton("Fit");
         auto *btnOne = new QPushButton("One");
         
-        btnFit->setStyleSheet("background-color: #444; border: 1px solid #555; padding: 5px 15px; border-radius: 3px;");
-        btnOne->setStyleSheet("background-color: #444; border: 1px solid #555; padding: 5px 15px; border-radius: 3px;");
+        btnPrev->setFocusPolicy(Qt::NoFocus);
+        btnNext->setFocusPolicy(Qt::NoFocus);
+        btnFit->setFocusPolicy(Qt::NoFocus);
+        btnOne->setFocusPolicy(Qt::NoFocus);
+        scrollArea->setFocusPolicy(Qt::NoFocus);
+        
+        QString btnStyle = "background-color: #444; border: 1px solid #555; padding: 5px 15px; border-radius: 3px;";
+        btnPrev->setStyleSheet(btnStyle);
+        btnNext->setStyleSheet(btnStyle);
+        btnFit->setStyleSheet(btnStyle);
+        btnOne->setStyleSheet(btnStyle);
 
+        bottomLayout->addWidget(btnPrev);
+        bottomLayout->addWidget(btnNext);
+        bottomLayout->addSpacing(20);
         bottomLayout->addWidget(btnFit);
         bottomLayout->addWidget(btnOne);
         bottomLayout->addStretch();
         
         mainLayout->addWidget(bottomBar);
 
+        connect(btnPrev, &QPushButton::clicked, this, &ImageViewer::previousImage);
+        connect(btnNext, &QPushButton::clicked, this, &ImageViewer::nextImage);
         connect(btnFit, &QPushButton::clicked, this, [this]() {
             setZoomState(ZoomState::Fit);
         });
@@ -62,18 +82,51 @@ public:
 
         zoomState = ZoomState::Fit;
         zoomLevel = 1.0;
+        currentImageIndex = 0;
+    }
+
+    void loadImages(const QStringList &fileNames) {
+        images = fileNames;
+        currentImageIndex = 0;
+        if (!images.isEmpty()) {
+            loadImage(images[currentImageIndex]);
+        } else {
+            imageLabel->setText("No images provided.");
+        }
     }
 
     void loadImage(const QString &fileName) {
         if (originalPixmap.load(fileName)) {
             setWindowTitle(QFileInfo(fileName).fileName() + " - Waffle");
-            updateImage();
+            setZoomState(ZoomState::Fit);
         } else {
-            imageLabel->setText("Failed to load image");
+            imageLabel->setText("Failed to load image: " + QFileInfo(fileName).fileName());
         }
     }
 
+    void nextImage() {
+        if (images.size() <= 1) return;
+        currentImageIndex = (currentImageIndex + 1) % images.size();
+        loadImage(images[currentImageIndex]);
+    }
+
+    void previousImage() {
+        if (images.size() <= 1) return;
+        currentImageIndex = (currentImageIndex - 1 + images.size()) % images.size();
+        loadImage(images[currentImageIndex]);
+    }
+
 protected:
+    void keyPressEvent(QKeyEvent *event) override {
+        if (event->key() == Qt::Key_Left) {
+            previousImage();
+        } else if (event->key() == Qt::Key_Right) {
+            nextImage();
+        } else {
+            QWidget::keyPressEvent(event);
+        }
+    }
+
     void showEvent(QShowEvent *event) override {
         QWidget::showEvent(event);
         // Force an update once the layout and viewport have their final initial sizes calculated
@@ -138,6 +191,17 @@ private:
             if (newSize.height() < 1) newSize.setHeight(1);
             scaledPixmap = originalPixmap.scaled(newSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
         }
+
+        // Handle RGBA images by adding a white background
+        if (scaledPixmap.hasAlpha()) {
+            QPixmap background(scaledPixmap.size());
+            background.fill(Qt::white);
+            QPainter painter(&background);
+            painter.drawPixmap(0, 0, scaledPixmap);
+            painter.end();
+            scaledPixmap = background;
+        }
+
         imageLabel->setPixmap(scaledPixmap);
         imageLabel->resize(scaledPixmap.size());
     }
@@ -147,6 +211,8 @@ private:
     QPixmap originalPixmap;
     ZoomState zoomState;
     double zoomLevel;
+    QStringList images;
+    int currentImageIndex;
 };
 
 int main(int argc, char *argv[])
@@ -156,9 +222,33 @@ int main(int argc, char *argv[])
     ImageViewer viewer;
 
     if (argc > 1) {
-        viewer.loadImage(argv[1]);
+        QStringList args;
+        if (argc == 2) {
+            QString path = QString::fromLocal8Bit(argv[1]);
+            QFileInfo checkFile(path);
+            if (checkFile.isDir()) {
+                QDir dir(path);
+                QStringList filters;
+                filters << "*.jpg" << "*.jpeg" << "*.jpe" << "*.jfif" << "*.png" << "*.gif" << "*.webp" 
+        << "*.bmp" << "*.dib" << "*.ico" << "*.tiff" << "*.tif" << "*.svg" << "*.svgz" 
+        << "*.heic" << "*.heif" << "*.avif" << "*.tga" << "*.psd" << "*.dng" << "*.hdr" 
+        << "*.exr" << "*.jp2" << "*.j2k" << "*.jpx" << "*.jpm" << "*.mj2" << "*.pnm" 
+        << "*.pbm" << "*.pgm" << "*.ppm" << "*.xbm" << "*.xpm" << "*.dds";
+                QFileInfoList list = dir.entryInfoList(filters, QDir::Files);
+                for (const QFileInfo &fileInfo : list) {
+                    args << fileInfo.absoluteFilePath();
+                }
+            } else {
+                args << path;
+            }
+        } else {
+            for (int i = 1; i < argc; ++i) {
+                args << QString::fromLocal8Bit(argv[i]);
+            }
+        }
+        viewer.loadImages(args);
     } else {
-        viewer.loadImage("");
+        viewer.loadImages(QStringList());
     }
 
     viewer.show();
